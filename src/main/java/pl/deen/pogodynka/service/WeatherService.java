db0a73d1f8ae46bb4398d48490f4ccd2d393acf0
Dominik <dominik-nowak@hotmail.com>
2019-11-24 07:45:12 +0100

    +appid moved to application.properties

--- src/main/java/pl/deen/pogodynka/service/WeatherService.java
+++ src/main/java/pl/deen/pogodynka/service/WeatherService.java
@@ -1,6 +1,7 @@
 package pl.deen.pogodynka.service;
 
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.deen.pogodynka.model.Weather;
@@ -9,11 +10,19 @@ import pl.deen.pogodynka.model.Weather;
 @NoArgsConstructor
 public class WeatherService {
 
+    @Value("${app-properties.appid}")
+    private String appid;
+
     public Weather getWeatherByTownName(String townName) {
         RestTemplate restTemplate = new RestTemplate();
-        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + townName + "&units=metric&appid=1a1c93e99537b52f1b9bc26a90fe61ba&lang=pl";
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="
+                + townName
+                + "&units=metric&lang=pl&appid="
+                + appid;
         Weather weather = restTemplate.getForObject(url, Weather.class);
-        weather.setImage("https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/" + weather.getDesc().get(0).getIcon() + ".png");
+        weather.setImage("https://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/"
+                + weather.getDesc().get(0).getIcon()
+                + ".png");
         return weather;
     }
 
